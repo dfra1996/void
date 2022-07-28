d3cdaea9decbe9ca36e75ee3cc4fa5821b749709
DuvanR <dfrobayo9@misena.edu.co>
2022-07-28 16:23:47 -0500

    Work 28/7

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<title>VOID</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta content="SisteDVN Sistema de ventas, intentario y prestamos" name="description">
+	<meta content="SisteDVN" name="Duvan Robayo">
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+	<!-- third party css -->
+	<link href="assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css">
+	<!-- third party css end -->
+
+	<!-- App css -->
+	<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="light-style">
+	<link href="assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style">
+
+	<title>SisteDVN</title>
 </head>
 <body>
+	
 	<header>
 	<?php 
 		$pg = isset($_REQUEST["pg"]) ? $_REQUEST["pg"]:NULL;
